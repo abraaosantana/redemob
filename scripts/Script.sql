a596c367336fe21ae56a9e73c72830d4d2d096ce
Abraão Santana <abraao.cs@gmail.com>
2024-02-21 22:14:12 -0300

    Ajuste data nascimento script.

--- scripts/Script.sql
+++ scripts/Script.sql
@@ -136,9 +136,9 @@ INSERT INTO rocket.seg_grupo(id, active, date_created, last_updated, name, versi
 VALUES (nextval('rocket.seg_grupo_seq'), TRUE, now(), now(), 'USER', 0);
 
 INSERT INTO rocket.seg_usuario(id, active, version, date_created, last_updated, cpf, email, password, data_nascimento, nome_completo, nome_completo_mae) 
-VALUES (nextval('rocket.seg_usuario_seq'), TRUE, 0, now(), now(),'87455814364', 'abraao.cs@gmail.com', '$2a$10$eqFU1oxf14wBDWMvHI9SDuopqwxUFPOLt5YfOyNzrxj4qG9fkOaGa','29/09/1986', 'Administrador do Sistema', 'Mãe Administrador do Sistema');
+VALUES (nextval('rocket.seg_usuario_seq'), TRUE, 0, now(), now(),'87455814364', 'abraao.cs@gmail.com', '$2a$10$eqFU1oxf14wBDWMvHI9SDuopqwxUFPOLt5YfOyNzrxj4qG9fkOaGa','1986-09-29', 'Administrador do Sistema', 'Mãe Administrador do Sistema');
 INSERT INTO rocket.seg_usuario(id, active, version, date_created, last_updated, cpf, email, password, data_nascimento, nome_completo, nome_completo_mae) 
-VALUES (nextval('rocket.seg_usuario_seq'), TRUE, 0, now(), now(), '61713047861', '2401acs@gmail.com', '$2a$10$eqFU1oxf14wBDWMvHI9SDuopqwxUFPOLt5YfOyNzrxj4qG9fkOaGa', '29/09/1986', 'Usuário do Sistema', 'Mãe Usuário do Sistema');
+VALUES (nextval('rocket.seg_usuario_seq'), TRUE, 0, now(), now(), '61713047861', '2401acs@gmail.com', '$2a$10$eqFU1oxf14wBDWMvHI9SDuopqwxUFPOLt5YfOyNzrxj4qG9fkOaGa', '1986-09-29', 'Usuário do Sistema', 'Mãe Usuário do Sistema');
 
 INSERT INTO rocket.seg_usuario_grupo(usuario_id, grupo_id) VALUES (1, 1);
 INSERT INTO rocket.seg_usuario_grupo(usuario_id, grupo_id) VALUES (1, 2);
